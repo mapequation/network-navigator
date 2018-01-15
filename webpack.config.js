c8dc1c61c9d62d80eaeb22836ff5ff0ca18dbb84
Anton Eriksson <aner0164@student.umu.se>
2018-01-15 09:33:46 +0100

    Moved to webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js')
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    resolve: {
+        alias: {
+            d3: 'd3/build/d3.js'
+        }
+    }
+};
