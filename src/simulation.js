2e87a9694f6f19501e1fa4b2120080865405c306
Anton Eriksson <aner0164@student.umu.se>
2018-04-23 13:21:46 +0200

    Make links with less flow weaker in the simulation.
    Also make the rest length dependent on the flow.

--- src/simulation.js
+++ src/simulation.js
@@ -3,15 +3,15 @@ import {
     forceCollide,
     forceLink,
     forceManyBody,
-    forceCenter
+    forceCenter,
+    scaleLinear
 } from 'd3';
 
 
 export default function Simulation({ x, y }, { charge, linkDistance }) {
     const simulation = forceSimulation()
         .force('collide', forceCollide(70))
-        .force('link', forceLink()
-            .distance(linkDistance))
+        .force('link', forceLink())
         .force('charge', forceManyBody()
             .strength(-charge)
             .distanceMax(400))
@@ -23,10 +23,25 @@ export default function Simulation({ x, y }, { charge, linkDistance }) {
     simulation.alphaDecay(1 - Math.pow(0.001, 1/nIterations));
 
     simulation.init = ({ nodes, links }) => {
+        const maxLinkFlow = links.reduce((max, link) => Math.max(max, link.flow), -Infinity);
+        const minLinkFlow = links.reduce((min, link) => Math.min(min, link.flow), Infinity);
+        const minDistance = 50;
+        const maxDistance = linkDistance;
+
+        const distance = scaleLinear().domain([minLinkFlow, maxLinkFlow]).range([maxDistance, minDistance]);
+
+        const force = scaleLinear().domain([minLinkFlow, maxLinkFlow]).range([0.5, 1]);
+
+        const defaultStrength = simulation.force('link').strength();
+
+        const linkStrength = link => force(link.flow) * defaultStrength(link);
+
         simulation
             .nodes(nodes)
             .force('link')
-            .links(links);
+            .links(links)
+            .distance(l => distance(l.flow))
+            .strength(linkStrength)
 
         const alpha = simulation.alpha();
 
