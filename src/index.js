09d5593b39a4d0c2febdccff9dd97b59091a46f1
Anton Eriksson <anton.eriksson@umu.se>
2019-05-28 10:50:06 +0200

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 import App from "./App";
 
 
 ReactDOM.render(<App/>, document.getElementById("root"));
-registerServiceWorker();
+unregister();
