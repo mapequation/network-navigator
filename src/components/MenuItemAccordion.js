3cf00520aacc29d220365c0eb8b4964b1af5bcad
Anton Eriksson <anton.eriksson@umu.se>
2019-06-03 13:22:55 +0200

    Make MenuItemAccordion a functional component

--- src/components/MenuItemAccordion.js
+++ src/components/MenuItemAccordion.js
@@ -1,29 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion, Icon, Menu, Popup } from "semantic-ui-react";
 
 
-export default class MenuItemAccordion extends React.PureComponent {
-  state = {
-    visible: this.props.visible || false
-  };
+export default function MenuItemAccordion(props) {
+  const { title, popup, children } = props;
+  const [visible, setVisible] = useState(props.visible || false);
 
-  toggle = () => this.setState({ visible: !this.state.visible });
+  const accordionTitle = <Accordion.Title active={visible} onClick={() => setVisible(!visible)}>
+    <Icon name='dropdown'/>{title}
+  </Accordion.Title>;
 
-  render() {
-    const { visible } = this.state;
-    const { title, popup, children } = this.props;
-
-    const accordionTitle = <Accordion.Title active={visible} onClick={this.toggle}>
-      <Icon name='dropdown'/>{title}
-    </Accordion.Title>;
-
-    return (
-      <Menu.Item as={Accordion}>
-        {popup ? <Popup trigger={accordionTitle} content={popup} size='tiny'/> : accordionTitle}
-        <Accordion.Content active={visible}>
-          {children}
-        </Accordion.Content>
-      </Menu.Item>
-    );
-  }
+  return (
+    <Menu.Item as={Accordion}>
+      {popup ? <Popup trigger={accordionTitle} content={popup} size='tiny'/> : accordionTitle}
+      <Accordion.Content active={visible}>
+        {children}
+      </Accordion.Content>
+    </Menu.Item>
+  );
 }
