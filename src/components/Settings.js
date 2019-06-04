4dcdd32126a70ed787fb44990bc459b25fb55e99
Anton Eriksson <anton.eriksson@umu.se>
2019-06-04 20:09:55 +0200

    Use slider checkbox instead of radio

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,13 @@
 import React, { useContext } from "react";
-import { Checkbox, Form, Radio } from "semantic-ui-react";
+import { Checkbox } from "semantic-ui-react";
 import Dispatch from "../context/Dispatch";
 
+
+const MyCheckbox = props => <Checkbox style={{ margin: "0.3em 0 0.3em 0" }} {...props}/>;
+
+const ToggleCheckbox = props => <MyCheckbox toggle {...props}/>;
+
+const SliderCheckbox = props => <MyCheckbox slider {...props}/>;
 
 export default function Settings(props) {
   const {
@@ -14,77 +20,32 @@ export default function Settings(props) {
   const { dispatch } = useContext(Dispatch);
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Field>Module size based on</Form.Field>
-        <Form.Field
-          control={Radio}
-          label='flow'
-          value='flow'
-          checked={nodeSize === "flow"}
-          onChange={(e, { value }) => dispatch({ type: "nodeSize", value })}
-        />
-        <Form.Field
-          control={Radio}
-          label='nodes'
-          value='nodes'
-          checked={nodeSize === "nodes"}
-          onChange={(e, { value }) => dispatch({ type: "nodeSize", value })}
-        />
-      </Form.Group>
-
-      <Form.Group>
-        <Form.Field>Module radius scale as</Form.Field>
-        <Form.Field
-          control={Radio}
-          label="root"
-          value="root"
-          checked={nodeScale === "root"}
-          onChange={(e, { value }) => dispatch({ type: "nodeScale", value })}
-        />
-        <Form.Field
-          control={Radio}
-          label="linear"
-          value="linear"
-          checked={nodeScale === "linear"}
-          onChange={(e, { value }) => dispatch({ type: "nodeScale", value })}
-        />
-      </Form.Group>
-
-      <Form.Group>
-        <Form.Field>Link width scale as</Form.Field>
-        <Form.Field
-          control={Radio}
-          label='root'
-          value='root'
-          checked={linkScale === "root"}
-          onChange={(e, { value }) => dispatch({ type: "linkScale", value })}
-        />
-        <Form.Field
-          control={Radio}
-          label='linear'
-          value='linear'
-          checked={linkScale === "linear"}
-          onChange={(e, { value }) => dispatch({ type: "linkScale", value })}
-        />
-      </Form.Group>
-
-      <Form.Group>
-        <Checkbox
-          toggle
-          onChange={(e, { checked }) => dispatch({ type: "labelsVisible", value: checked })}
-          checked={labelsVisible}
-          label="Show labels"
-        />
-      </Form.Group>
-      <Form.Group>
-        <Checkbox
-          toggle
-          onChange={(e, { checked }) => dispatch({ type: "simulationEnabled", value: checked })}
-          checked={simulationEnabled}
-          label="Enable simulation"
-        />
-      </Form.Group>
-    </Form>
+    <React.Fragment>
+      <SliderCheckbox
+        label={`Module size based on: ${nodeSize}`}
+        checked={nodeSize === "flow"}
+        onChange={(e, { checked }) => dispatch({ type: "nodeSize", value: checked ? "flow" : "nodes" })}
+      />
+      <SliderCheckbox
+        label={`Module radius scale: ${nodeScale}`}
+        checked={nodeScale === "root"}
+        onChange={(e, { checked }) => dispatch({ type: "nodeScale", value: checked ? "root" : "linear" })}
+      />
+      <SliderCheckbox
+        label={`Link width scale: ${linkScale}`}
+        checked={linkScale === "root"}
+        onChange={(e, { checked }) => dispatch({ type: "linkScale", value: checked ? "root" : "linear" })}
+      />
+      <ToggleCheckbox
+        label="Show labels"
+        checked={labelsVisible}
+        onChange={(e, { checked }) => dispatch({ type: "labelsVisible", value: checked })}
+      />
+      <ToggleCheckbox
+        label="Enable simulation"
+        checked={simulationEnabled}
+        onChange={(e, { checked }) => dispatch({ type: "simulationEnabled", value: checked })}
+      />
+    </React.Fragment>
   );
 };
