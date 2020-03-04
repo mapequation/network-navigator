90a3b386ff361190daf025a2209e1c2806980101
Anton Eriksson <anton.eriksson@umu.se>
2020-03-04 08:43:54 +0100

    Only show enter/exit flow if defined

--- src/components/SelectedNode.js
+++ src/components/SelectedNode.js
@@ -74,6 +74,7 @@ export default function SelectedNode(props) {
           />
           <Table.Cell content={(+node.flow).toPrecision(4)}/>
         </Table.Row>
+        {node.enterFlow != null &&
         <Table.Row>
           <Popup
             trigger={<Table.Cell content='Enter flow'/>}
@@ -82,6 +83,8 @@ export default function SelectedNode(props) {
           />
           <Table.Cell content={(+node.enterFlow).toPrecision(4)}/>
         </Table.Row>
+        }
+        {node.exitFlow != null &&
         <Table.Row>
           <Popup
             trigger={<Table.Cell content='Exit flow'/>}
@@ -90,6 +93,7 @@ export default function SelectedNode(props) {
           />
           <Table.Cell content={(+node.exitFlow).toPrecision(4)}/>
         </Table.Row>
+        }
         {isRoot && directed &&
         <Table.Row>
           <Popup
