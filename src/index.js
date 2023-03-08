91fced2792660824c01ae6d34f640d837b5289a7
Anton Holmgren <anton.holmgren@umu.se>
2023-03-08 14:37:58 +0100

    build(ci): Remove sentry

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import "core-js";
-import * as Sentry from "@sentry/browser";
 import React from "react";
 import { hydrate, render } from "react-dom";
 import App from "./components/App";
 import "./index.css";
-
-
-const dsn = process.env.NODE_ENV === "production" ? "https://a095cdb615de4acfaa7fce76eb9c60da@sentry.io/1763309" : "";
-
-Sentry.init({
-  dsn,
-  beforeSend(event) {
-    // Check if it is an exception, and if so, show the report dialog
-    if (event.exception) {
-      Sentry.showReportDialog({
-        eventId: event.event_id,
-        subtitle2: "If you'd like to help, tell us what happened below. Your network data stay on your computer."
-      });
-    }
-    return event;
-  }
-});
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
